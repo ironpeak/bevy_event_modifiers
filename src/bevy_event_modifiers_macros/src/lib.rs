6be9dec0c10a101856dbcb9069bce97025b6ab00
Hrafn Orri Hrafnkelsson <hrafn@vidfjord.is>
2024-08-01 18:39:44 +0000

    update

--- src/bevy_event_modifiers_macros/src/lib.rs
+++ src/bevy_event_modifiers_macros/src/lib.rs
@@ -3,11 +3,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::{quote, ToTokens};
-use syn::{
-    parse::Parse, parse_macro_input, punctuated::Punctuated, token::Comma,
-    AngleBracketedGenericArguments, Data, DeriveInput, Field, GenericArgument, Ident,
-    PathArguments, Type,
-};
+use syn::{parse_macro_input, Data, DeriveInput, Ident, Type};
 
 #[proc_macro_derive(EventModifierContext)]
 pub fn derive_event_modifier_context(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -29,16 +25,21 @@ pub fn derive_event_modifier_context(input: proc_macro::TokenStream) -> proc_mac
         .expect("Struct name must end with 'ModifierContext'");
 
     let input_ty = Ident::new(&format!("{}In", base_name), Span::call_site());
+    let modifier_ty = Ident::new(&format!("{}Modifier", base_name), Span::call_site());
+    let priority_ty = Ident::new(&format!("{}ModifierPriority", base_name), Span::call_site());
+    let metadata_ty = Ident::new(&format!("{}ModifierMetadata", base_name), Span::call_site());
+    let output_ty = Ident::new(&format!("{}Out", base_name), Span::call_site());
 
     let system_param_names = data.fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         quote! {
-            #field_ident: &#field_ident,
+            #field_ident
         }
     });
 
     let system_params = data.fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().expect("Field must have an identifier");
+
         // TODO: would prefer to do this logic using AST
         let field_ty_str = format!("{}", field.ty.to_token_stream())
             .replace("\n", "")
@@ -51,12 +52,10 @@ pub fn derive_event_modifier_context(input: proc_macro::TokenStream) -> proc_mac
             .replace(" >", ">")
             .replace(", ", ",")
             .replace(" ,", ",")
-            .strip_prefix("&")
-            .expect("Failed to strip prefix")
-            .replace("'state ", "")
-            .replace("'world ", "")
-            .replace("'state,", "")
-            .replace("'world,", "");
+            .replace("'s ", "")
+            .replace("'w ", "")
+            .replace("'s,", "")
+            .replace("'w,", "");
 
         let field_ty = syn::parse_str::<Type>(&field_ty_str).expect("Failed to parse type");
 
@@ -65,16 +64,11 @@ pub fn derive_event_modifier_context(input: proc_macro::TokenStream) -> proc_mac
         }
     });
 
-    let modifier_ty = Ident::new(&format!("{}Modifier", base_name), Span::call_site());
-    let priority_ty = Ident::new(&format!("{}ModifierPriority", base_name), Span::call_site());
-
-    let output_ty = Ident::new(&format!("{}Out", base_name), Span::call_site());
-
     let output = quote! {
         #[derive(bevy_ecs::prelude::Component)]
         pub struct #modifier_ty {
             pub priority: #priority_ty,
-            pub modify: fn(&mut #struct_name, &mut #output_ty),
+            pub modify: fn(&mut #struct_name, &mut #metadata_ty, &mut #output_ty),
         }
 
         impl Ord for #modifier_ty {
@@ -100,212 +94,31 @@ pub fn derive_event_modifier_context(input: proc_macro::TokenStream) -> proc_mac
         }
 
         impl #user_impl_generics #struct_name #user_impl_generics {
-            fn system #user_impl_generics (
+            fn system(
                 mut p_events_in: bevy_ecs::prelude::EventReader<#input_ty>,
                 #(#system_params),*,
                 p_modifiers: bevy_ecs::prelude::Query<&#modifier_ty>,
                 mut p_events_out: bevy_ecs::prelude::EventWriter<#output_ty>,
             ) {
-                let context = #struct_name {
-                    #(#system_param_names),*
+                let mut context = CombatEventModifierContext {
+                    #(#system_param_names),*,
                 };
                 let modifiers = p_modifiers
                     .iter()
                     .sort::<&#modifier_ty>()
                     .collect::<Vec<_>>();
                 for event in p_events_in.read() {
+                    let mut metadata = CombatEventModifierMetadata::init(&mut context, event);
+                    let mut event_out = CombatEventOut::init(&mut context, event);
                     for modifier in &modifiers {
+                        (modifier.modify)(&mut context, &mut metadata, &mut event_out);
                     }
+                    p_events_out.send(event_out);
                 }
             }
         }
 
         impl #user_impl_generics bevy_event_modifiers::prelude::EventModifierContext for #struct_name #user_impl_generics {
-            fn register_type(app: &mut bevy_app::prelude::App) -> &mut bevy_app::prelude::App {
-                app.add_event::<#input_ty>();
-                app.add_event::<#output_ty>();
-                app.add_systems(bevy_app::prelude::Update, #struct_name ::system);
-                app
-            }
-        }
-    };
-
-    TokenStream::from(output)
-}
-
-#[proc_macro_derive(EventModifier)]
-pub fn derive_event_modifier(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
-
-    let (user_impl_generics, _, _) = ast.generics.split_for_impl();
-
-    let struct_name = &ast.ident;
-
-    let data = match ast.data {
-        Data::Struct(data) => data,
-        _ => panic!("Only structs are supported"),
-    };
-
-    let input_field = data
-        .fields
-        .iter()
-        .find(|field| {
-            field
-                .ident
-                .as_ref()
-                .map(|ident| ident == "input")
-                .unwrap_or(false)
-        })
-        .expect("No input field found");
-
-    let priority_field = data
-        .fields
-        .iter()
-        .find(|field| {
-            field
-                .ident
-                .as_ref()
-                .map(|ident| ident == "priority")
-                .unwrap_or(false)
-        })
-        .expect("No priority field found");
-
-    let context_fields: Vec<&Field> = data
-        .fields
-        .iter()
-        .filter(|field| {
-            field
-                .ident
-                .as_ref()
-                .map(|ident| ident != "input" && ident != "priority" && ident != "output")
-                .unwrap_or(false)
-        })
-        .collect();
-
-    let output_field = data
-        .fields
-        .iter()
-        .find(|field| {
-            field
-                .ident
-                .as_ref()
-                .map(|ident| ident == "output")
-                .unwrap_or(false)
-        })
-        .expect("No output field found");
-
-    let input_ty = &input_field.ty;
-
-    let priority_ty = &priority_field.ty;
-
-    let struct_context_name = Ident::new(&format!("{}Context", struct_name), Span::call_site());
-    let struct_context_fields = context_fields.iter().map(|field| {
-        let field_ident = field.ident.as_ref().unwrap();
-        let field_ty = &field.ty;
-        quote! {
-            pub #field_ident: #field_ty
-        }
-    });
-
-    let system_context_params = context_fields.iter().map(|field| {
-        let field_ident = field.ident.as_ref().unwrap();
-        let field_ty = match &field.ty {
-            Type::Path(reference) => match reference.path.segments[0].ident.to_string().as_str() {
-                "Query" => {
-                    let args = match &reference.path.segments[0].arguments {
-                        PathArguments::AngleBracketed(args) => {
-                            let mut param_args: Punctuated<GenericArgument, Comma> =
-                                Punctuated::new();
-                            for arg in &args.args {
-                                if matches!(arg, GenericArgument::Lifetime(_)) {
-                                    continue;
-                                }
-                                param_args.push(arg.clone());
-                            }
-                            param_args
-                        }
-                        _ => panic!("Only angle bracketed arguments are supported"),
-                    };
-                    let mut field_ty = field.ty.clone();
-                    match &mut field_ty {
-                        Type::Path(reference) => {
-                            reference.path.segments[0].arguments =
-                                PathArguments::AngleBracketed(AngleBracketedGenericArguments {
-                                    colon2_token: None,
-                                    lt_token: Default::default(),
-                                    args: args,
-                                    gt_token: Default::default(),
-                                });
-                        }
-                        _ => panic!("Only paths are supported"),
-                    }
-                    field_ty
-                }
-                _ => field.ty.clone(),
-            },
-            _ => panic!("Only references are supported"),
-        };
-        quote! {
-            #field_ident: #field_ty
-        }
-    });
-
-    let struct_modifier_name = Ident::new(&format!("{}Modifier", struct_name), Span::call_site());
-
-    let output_ty = &output_field.ty;
-
-    let output = quote! {
-        pub struct #struct_context_name #user_impl_generics {
-            #(#struct_context_fields),*
-        }
-
-        #[derive(bevy_ecs::prelude::Component)]
-        pub struct #struct_modifier_name {
-            pub priority: #priority_ty,
-            pub modify: fn(&mut #struct_context_name, &mut #output_ty),
-        }
-
-        impl Ord for #struct_modifier_name {
-            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-                self.priority.cmp(&other.priority)
-            }
-        }
-
-        impl PartialOrd for #struct_modifier_name {
-            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-                self.priority.partial_cmp(&other.priority)
-            }
-        }
-
-        impl Eq for #struct_modifier_name {
-
-        }
-
-        impl PartialEq for #struct_modifier_name {
-            fn eq(&self, other: &Self) -> bool {
-                self.priority == other.priority
-            }
-        }
-
-        impl #user_impl_generics #struct_name #user_impl_generics {
-            fn system(
-                mut events_in: bevy_ecs::prelude::EventReader<#input_ty>,
-                #(#system_context_params),*,
-                modifiers: bevy_ecs::prelude::Query<&#struct_modifier_name>,
-                mut events_out: bevy_ecs::prelude::EventWriter<#output_ty>,
-            ) {
-                let modifiers = modifiers
-                    .iter()
-                    .sort::<&#struct_modifier_name>()
-                    .collect::<Vec<_>>();
-                for event in events_in.read() {
-                    for modifier in &modifiers {
-                    }
-                }
-            }
-        }
-
-        impl #user_impl_generics bevy_event_modifiers::prelude::EventModifier for #struct_name #user_impl_generics {
             fn register_type(app: &mut bevy_app::prelude::App) -> &mut bevy_app::prelude::App {
                 app.add_event::<#input_ty>();
                 app.add_event::<#output_ty>();
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,38 +1,7 @@
-// use bevy_app::prelude::*;
-// use bevy_ecs::prelude::*;
-// use bevy_event_modifiers::prelude::*;
-// use bevy_event_modifiers_macros::{EventModifier, EventModifierContext};
-
-// #[derive(Component)]
-// pub struct ExampleComponent;
-
-// #[derive(Event)]
-// pub struct CombatEventIn;
-
-// #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-// pub enum CombatEventModifierPriority {
-//     Low,
-//     Medium,
-//     High,
-// }
-
-// #[derive(EventModifierContext)]
-// pub struct CombatEventModifierContext<'w, 's> {
-//     pub entities:
-//         &'s Query<'w, 's, (&'s ExampleComponent, &'s ExampleComponent)>,
-// }
-
-// #[derive(Event)]
-// pub struct CombatEventOut;
-
-// pub fn init(app: &mut App) {
-//     // app.add_event_with_modifiers::<CombatEvent<'_, '_>>();
-// }
-
 use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
 use bevy_event_modifiers::prelude::*;
-use bevy_event_modifiers_macros::{EventModifier, EventModifierContext};
+use bevy_event_modifiers_macros::EventModifierContext;
 
 #[derive(Component)]
 pub struct ExampleComponent;
@@ -46,6 +15,15 @@ pub struct ExampleEvent;
 #[derive(Event)]
 pub struct CombatEventIn;
 
+#[derive(Event)]
+pub struct CombatEventOut;
+
+impl CombatEventOut {
+    fn init(context: &mut CombatEventModifierContext, event: &CombatEventIn) -> Self {
+        CombatEventOut {}
+    }
+}
+
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum CombatEventModifierPriority {
     Low,
@@ -56,11 +34,12 @@ pub enum CombatEventModifierPriority {
 pub struct CombatEventModifierMetadata {}
 
 impl CombatEventModifierMetadata {
-    fn new(context: &mut CombatEventModifierContext, event: &CombatEventIn) -> Self {
+    fn init(context: &mut CombatEventModifierContext, event: &CombatEventIn) -> Self {
         CombatEventModifierMetadata {}
     }
 }
 
+#[derive(EventModifierContext)]
 pub struct CombatEventModifierContext<'w, 's> {
     pub entities: Query<'w, 's, (&'static ExampleComponent, &'static ExampleComponent)>,
     pub resource_mut: ResMut<'w, ExampleResource>,
@@ -68,92 +47,7 @@ pub struct CombatEventModifierContext<'w, 's> {
     pub event_reader: EventReader<'w, 's, ExampleEvent>,
     pub event_writer: EventWriter<'w, ExampleEvent>,
 }
-pub struct CombatEventModifier {
-    pub priority: CombatEventModifierPriority,
-    pub modify:
-        fn(&mut CombatEventModifierContext, &mut CombatEventModifierMetadata, &mut CombatEventOut),
-}
-impl bevy_ecs::component::Component for CombatEventModifier
-where
-    Self: Send + Sync + 'static,
-{
-    const STORAGE_TYPE: bevy_ecs::component::StorageType = bevy_ecs::component::StorageType::Table;
-}
-impl Ord for CombatEventModifier {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.priority.cmp(&other.priority)
-    }
-}
-impl PartialOrd for CombatEventModifier {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.priority.partial_cmp(&other.priority)
-    }
-}
-impl Eq for CombatEventModifier {}
-impl PartialEq for CombatEventModifier {
-    fn eq(&self, other: &Self) -> bool {
-        self.priority == other.priority
-    }
-}
-impl<'w, 's> CombatEventModifierContext<'w, 's> {
-    fn system(
-        mut p_events_in: bevy_ecs::prelude::EventReader<CombatEventIn>,
-        entities: Query<(&'static ExampleComponent, &'static ExampleComponent)>,
-        resource_mut: ResMut<ExampleResource>,
-        resource: Res<ExampleResource>,
-        event_reader: EventReader<ExampleEvent>,
-        event_writer: EventWriter<ExampleEvent>,
-        p_modifiers: bevy_ecs::prelude::Query<&CombatEventModifier>,
-        mut p_events_out: bevy_ecs::prelude::EventWriter<CombatEventOut>,
-    ) {
-        let mut context = CombatEventModifierContext {
-            entities,
-            resource_mut,
-            resource,
-            event_reader,
-            event_writer,
-        };
-        let modifiers = p_modifiers
-            .iter()
-            .sort::<&CombatEventModifier>()
-            .collect::<Vec<_>>();
-        for event in p_events_in.read() {
-            let mut metadata = CombatEventModifierMetadata::new(&mut context, event);
-            let mut event_out = CombatEventOut::new(&mut context, event);
-            for modifier in &modifiers {
-                (modifier.modify)(&mut context, &mut metadata, &mut event_out);
-            }
-            p_events_out.send(event_out);
-        }
-    }
-}
-impl<'w, 's> bevy_event_modifiers::prelude::EventModifierContext
-    for CombatEventModifierContext<'w, 's>
-{
-    fn register_type(app: &mut bevy_app::prelude::App) -> &mut bevy_app::prelude::App {
-        app.add_event::<CombatEventIn>();
-        app.add_event::<CombatEventOut>();
-        app.add_systems(
-            bevy_app::prelude::Update,
-            CombatEventModifierContext::system,
-        );
-        app
-    }
-}
-pub struct CombatEventOut;
-impl bevy_ecs::event::Event for CombatEventOut where Self: Send + Sync + 'static {}
-impl bevy_ecs::component::Component for CombatEventOut
-where
-    Self: Send + Sync + 'static,
-{
-    const STORAGE_TYPE: bevy_ecs::component::StorageType =
-        bevy_ecs::component::StorageType::SparseSet;
-}
 
-impl CombatEventOut {
-    fn new(context: &mut CombatEventModifierContext, event: &CombatEventIn) -> Self {
-        CombatEventOut {}
-    }
+pub fn init(app: &mut App) {
+    app.add_event_with_modifiers::<CombatEventModifierContext<'_, '_>>();
 }
-
-pub fn init(app: &mut App) {}
